1810ecdb64a140abc9515599faa8bc4152d62390
Alejandro Wu <alejandrowu189@gmail.com>
2020-09-17 21:04:51 -0400

    La version 1

---
+++ Fibonnacci/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Fibbonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.WriteLine("Escriba el digito para la cantidad de sucesion: ");
+                    int Cantidad = Convert.ToInt32(Console.ReadLine());
+
+                    double[] Fib = new double[2];
+                    Fib[0] = 1;
+                    Fib[1] = 1;
+                    int Num = 1;
+                    Console.WriteLine("La sucesion Fibonacci es: ");
+                    while (Num <= Cantidad)
+                    {
+                        Console.WriteLine(Fib[0]);
+                        double Temp = Fib[1] + Fib[0];
+                        Fib[0] = Fib[1];
+                        Fib[1] = Temp;
+
+                        Num++;
+                    }
+                    Console.ReadKey();
+        }
+    }
+}
---
+++ NumerosPrimos/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace NumerosPrimos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                int Z = 0;
+                int X = 0;
+
+                    Console.WriteLine("Digite un numero: ");
+                    Z = int.Parse(Console.ReadLine());
+
+                    for (int i = 1; i < Z; i++)
+                    {
+                        if (Z % i == 0)
+                        {
+                            X = X + 1;
+                        }
+                    }
+                    if (X >= 2)
+                    {
+                        Console.WriteLine(" {0} No es un numero primo. ", Z);
+                    }
+                    else
+                    {
+                        Console.WriteLine(" {0} Es un numero primo. ", Z);
+                    }
+
+                    Console.ReadKey();
+        }
+    }
+}
